c21602214e17dcf1d6bd5e4667ca1611dcbaa9de
delano <delano.mandelbaum@gmail.com>
2013-01-21 14:35:28 -0800

    Updated metrics tryout for 12h

--- try/23_range_metrics_try.rb
+++ try/23_range_metrics_try.rb
@@ -21,13 +21,13 @@ Stella.load! :tryouts
 [@m.past_1h.key, @m.past_1h.class, @m.past_1h.options[:expiration]]
 #=> ["v3:context:metric_id:past_1h", Redis::HashKey, 604800]
 
-## Has hash for past hour
+## Has hash for past 4 hours
 [@m.past_4h.key, @m.past_4h.class, @m.past_4h.options[:expiration]]
 #=> ["v3:context:metric_id:past_4h", Redis::HashKey, 604800]
 
-## Has hash for past hour
-[@m.past_24h.key, @m.past_24h.class, @m.past_24h.options[:expiration]]
-#=> ["v3:context:metric_id:past_24h", Redis::HashKey, 604800]
+## Has hash for past 12 hours
+[@m.past_12h.key, @m.past_12h.class, @m.past_12h.options[:expiration]]
+#=> ["v3:context:metric_id:past_12h", Redis::HashKey, 604800]
 
 ## Has lock
 [@m.lock.key, @m.lock.class, @m.lock.options[:expiration], @m.lock.options[:timeout]]
