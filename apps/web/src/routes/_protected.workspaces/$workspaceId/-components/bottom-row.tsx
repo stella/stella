6ada711f0da2782de57e7012ef31d23a4f66a56e
Damian Struk <damianstruk@outlook.com>
2026-05-05 12:24:26 +0200

    add double border to bottom row

--- apps/web/src/routes/_protected.workspaces/$workspaceId/-components/bottom-row.tsx
+++ apps/web/src/routes/_protected.workspaces/$workspaceId/-components/bottom-row.tsx
@@ -32,7 +32,7 @@ export const BottomRow = ({
         // oxlint-disable-next-line jsx_a11y/prefer-tag-over-role
         <WorkspaceGridRow className="cursor-pointer" role="button">
           <WorkspaceGridCell
-            className="z-10 flex items-center justify-center"
+            className="z-10 flex items-center justify-center border-t-2"
             style={{
               left: table.getColumn(selectColId)?.getStart("left"),
               right: table.getColumn(selectColId)?.getStart("right"),
@@ -42,7 +42,7 @@ export const BottomRow = ({
             <PlusIcon className="size-4" />
           </WorkspaceGridCell>
           <WorkspaceGridCell
-            className="z-10 flex items-center border-e-0"
+            className="z-10 flex items-center border-e-0 border-t-2"
             style={{
               left: table.getColumn(selectColId)?.getSize(),
               right: table.getColumn(selectColId)?.getSize(),
@@ -53,6 +53,7 @@ export const BottomRow = ({
           </WorkspaceGridCell>
           <WorkspaceGridCell
             aria-hidden="true"
+            className="border-t-2"
             role="presentation"
             style={{ gridColumn: "3 / -1" }}
           />
