d7019e7a44df1f5594ab6130cbea6e15ca5196de
jan-kubica <jankubica@protonmail.com>
2026-07-09 12:11:08 +0200

    fix: centralize native typescript compiler

---
+++ packages/scripts/src/tsc-native.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env bun
+
+import { spawnSync } from "node:child_process";
+import { join } from "node:path";
+
+const tsc = join(import.meta.dir, "../../../node_modules/@typescript/native/bin/tsc");
+
+const result = spawnSync(process.execPath, [tsc, ...process.argv.slice(2)], {
+  stdio: "inherit",
+});
+
+process.exit(result.status ?? 1);
