1034691f16055dcd2fd683ad722d1a71e02eddc2
jan-kubica <jankubica@protonmail.com>
2026-07-31 01:37:10 +0200

    Revert "fix: preserve metadata query input"
    This reverts commit 1bcb8f94c423fc74be04414038947deb7cb19a45.

--- apps/web/src/routes/_protected.workspaces/$workspaceId/-components/files/queries.ts
+++ apps/web/src/routes/_protected.workspaces/$workspaceId/-components/files/queries.ts
@@ -62,6 +62,7 @@ type FileOptionsProps = QueryOptionsInput<FileByFieldIdKey>;
 export const fileMetadataOptions = (props: FileOptionsProps) =>
   queryOptions({
     queryKey: filesKeys.metadataByFieldId(props),
+    enabled: props.enabled ?? true,
     retry: shouldRetryAPIRequest,
     queryFn: async ({ signal }) => {
       const response = await api
