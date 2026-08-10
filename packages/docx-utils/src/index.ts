e573f31ae6c0721c63763347ebadf6f28c3606e8
Jan Kubica <jankubica@protonmail.com>
2026-08-10 15:53:48 +0200

    fix: make DOCX source exports Node-loadable (#1866)
    * fix: make docx source exports Node-loadable
    
    * fix: allow TypeScript source import extensions in consumers

--- packages/docx-utils/src/index.ts
+++ packages/docx-utils/src/index.ts
@@ -1,13 +1,13 @@
-export { OOXML_NS, type OoxmlPrefix } from "./namespaces";
+export { OOXML_NS, type OoxmlPrefix } from "./namespaces.ts";
 export {
   DOCX_COMPRESSION,
   loadDocx,
   extractText,
   extractBinary,
   repackZip,
-} from "./zip";
+} from "./zip.ts";
 export {
   findNextRId,
   ensureContentType,
   ensureRelationship,
-} from "./relationships";
+} from "./relationships.ts";
